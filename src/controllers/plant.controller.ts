530207b91e6f5b2d5d7f544783ca7e114e0a9f65
Scopel_Yan <160073076+ScopelYann@users.noreply.github.com>
2026-04-15 12:01:18 -0300

    family find - add

--- src/controllers/plant.controller.ts
+++ src/controllers/plant.controller.ts
@@ -3,12 +3,12 @@ import { PlantService } from "../services/plant.service";
 import { StatusCode } from "status-code-enum";
 
 class PlantController {
-    async index(_req: Request, res: Response, _next: NextFunction) {
+    async indexAll(req: Request, res: Response, _next: NextFunction) {
         const service = new PlantService();
-        const page = Number(_req.params.page)
+        const page = Number(req.params.page) || 1;
 
         try {
-            const plants = await service.get(page);
+            const plants = await service.getAll(page);
 
             return res.status(StatusCode.SuccessOK).json(plants);
         } catch (error) {
@@ -16,6 +16,31 @@ class PlantController {
 
             return res.status(StatusCode.ServerErrorInternal).json({
                 message: "Error while trying to get plants",
+                error: error instanceof Error ? error.message : "Unknown error"
+            });
+        }
+    }
+
+    async indexFamily(req: Request, res: Response, _next: NextFunction) {
+        const service = new PlantService();
+        const page = Number(req.params.page) || 1;
+        const family = req.params.family;
+
+        try {
+            if (!family) {
+                return res.status(StatusCode.ClientErrorBadRequest).json({
+                    message: "Family parameter is required"
+                });
+            }
+
+            const filterPlants = await service.getByFamily(family, page);
+
+            return res.status(StatusCode.SuccessOK).json(filterPlants);
+        } catch (error) {
+            console.error("CONTROLLER ERROR:", error);
+
+            return res.status(StatusCode.ServerErrorInternal).json({
+                message: "Error while trying to get plants by family",
                 error: error instanceof Error ? error.message : "Unknown error"
             });
         }
--- src/routes/plant.routes.ts
+++ src/routes/plant.routes.ts
@@ -5,5 +5,10 @@ import plantController from "../controllers/plant.controller";
 export const routes = Router();
 
 routes.get("/get_plant/:page", (req, res, next) => {
-    return plantController.index(req, res, next);
+    return plantController.indexAll(req, res, next);
 });
+
+
+routes.get("/get_family/:family", (req, res, next) => {
+    return plantController.indexFamily(req, res, next)
+})
